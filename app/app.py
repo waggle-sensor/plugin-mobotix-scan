9eca09e7459f7b75024e716e167bf6c5169437c4
Bhupendra Raut <braut@anl.gov>
2023-07-25 13:30:33 -0500

    class added;func removed

--- app/app.py
+++ app/app.py
@@ -22,6 +22,7 @@ from select import select
 
 import timeout_decorator
 from waggle.plugin import Plugin
+import MobotixControl
 
 # camera image fetch timeout (seconds)
 DEFAULT_CAMERA_TIMEOUT = 120
@@ -42,75 +43,6 @@ def append_path(filename, string):
     '''
     filepath = Path(filename)
     return filepath.parent / (filepath.stem + string + filepath.suffix)
-
-# Dictionary contains "presets" which maps preset position numbers to the 
-# corresponding string for curl command for that position. 
-presets = {
-        1: "%FF%01%00%07%00%01%09",
-        2: "%FF%01%00%07%00%02%0A",
-        3: "%FF%01%00%07%00%03%0B",
-        4: "%FF%01%00%07%00%04%0C",
-        5: "%FF%01%00%07%00%05%0D",
-        6: "%FF%01%00%07%00%06%0E",
-        7: "%FF%01%00%07%00%07%0F",
-        8: "%FF%01%00%07%00%08%10",
-        9: "%FF%01%00%07%00%09%11",
-        10: "%FF%01%00%07%00%10%18",
-        11:"%FF%01%00%07%00%11%19",
-        12:"%FF%01%00%07%00%12%1A",
-        13:"%FF%01%00%07%00%13%1B",
-        14:"%FF%01%00%07%00%14%1C",
-        15:"%FF%01%00%07%00%15%1D",
-        16:"%FF%01%00%07%00%16%1E",
-        17:"%FF%01%00%07%00%17%1F",
-        18:"%FF%01%00%07%00%18%20",
-        19:"%FF%01%00%07%00%19%21",
-        20:"%FF%01%00%07%00%20%28",
-        21:"%FF%01%00%07%00%21%29",
-        22:"%FF%01%00%07%00%22%2A",
-        23:"%FF%01%00%07%00%23%2B",
-        24:"%FF%01%00%07%00%24%2C",
-        25:"%FF%01%00%07%00%25%2D",
-        26:"%FF%01%00%07%00%26%2E",
-        27:"%FF%01%00%07%00%27%2F",
-        28:"%FF%01%00%07%00%28%30",
-        29:"%FF%01%00%07%00%29%31",
-        30:"%FF%01%00%07%00%30%38",
-        31:"%FF%01%00%07%00%31%39",
-        32:"%FF%01%00%07%00%32%3A"
-    }
-
-
-# Move to only single preset position (Does not report to the beehive)
-def move_to_preset(pt_id, args):
-    '''
-    This function sends the curl command for the given preset position to the camera via subprocess. 
-    It returns the result of the "curl" command. 
-    '''
-    preset_code = presets.get(pt_id)
-    if not preset_code:
-        print("Invalid preset number")
-        return -1
-
-    cmd = ["curl",
-        "-u",
-        args.user+':'+args.password,
-        "-X",
-        "POST",
-        "http://{}/control/rcontrol?action=putrs232&rs232outtext=".format(args.ip)+preset_code]
-
-    #print(cmd)
-
-    try:
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        return result.stdout
-
-    except subprocess.CalledProcessError as e:
-        print("Error: {}".format(e))
-        return e
-
-    return 0
-
 
 
 def extract_timestamp_and_filename(path: Path):
@@ -190,6 +122,9 @@ def get_camera_frames(args):
 def main(args):
     loops = 0
 
+    # Instantiate the MobotixControl class for movement of the camera
+    mobot_ctl = MobotixControl(args.user, args.password, args.ip)
+
     with Plugin() as plugin:
         while loop_check(loops, args.loops):
             loops = loops + 1
@@ -202,7 +137,8 @@ def main(args):
             for move_pos in args.preset:
                 if args.preset[0]!=0:
                     # Move the caemra if scan is requested
-                    status = move_to_preset(move_pos, args)
+                    status = mobot_ctl.move_to_preset(move_pos)
+                    
                     plugin.publish('mobotix.move.status', status)
 
                     if status.strip() != str('OK'):
