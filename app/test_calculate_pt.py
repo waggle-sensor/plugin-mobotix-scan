66a009286302ab95de6e45b7aeffd4fbd0b36f2a
Bhupendra Raut <braut@anl.gov>
2024-08-16 14:30:56 -0500

    test added

---
+++ app/test_calculate_pt.py
@@ -0,0 +1,16 @@
+import unittest
+from MobotixScan import calculate_pt
+
+class TestCalculatePt(unittest.TestCase):
+    def test_case_1(self):
+        sdir = 28
+        pdir = "SES,NEG"
+        expected = '21, 16'
+        self.assertEqual(calculate_pt(sdir, pdir), expected)
+
+    def test_invalid_direction(self):
+        with self.assertRaises(ValueError) as context:
+            calculate_pt('GFH', '1')
+
+if __name__ == '__main__':
+    unittest.main()
